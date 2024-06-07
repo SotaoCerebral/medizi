8f4b94265a886bf08d2c546b3aff028b049729a3
SotãoCerebral <130693907+SotaoCerebral@users.noreply.github.com>
2024-06-07 17:24:00 -0300

    Delete medizi directory

--- medizi/src/main/java/com/example/medizi/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medizi.repository;
-
-import com.example.medizi.entity.endereco.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
-}
